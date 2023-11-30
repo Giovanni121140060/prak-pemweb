c52e82aacaa8ed00cb5ed43a3f410255932282f5
lazy <92906302+steverogecrs@users.noreply.github.com>
2023-11-30 23:42:35 +0700

    Create 121140060_giovanni lucy_prak7.php

---
+++ prak7/121140060_giovanni lucy_prak7.php
@@ -0,0 +1,31 @@
+<?php 
+
+// by Giovanni Lucy - 121140060 - Prak RC
+
+
+class mahasiswa {
+  public $nama;
+  public $nim;
+  public $prodi;
+
+  public function __construct($nama, $nim, $prodi) {
+    $this->nama = $nama;
+    $this->nim = $nim;
+    $this->prodi = $prodi;
+  }
+
+  public function showMahasiswa() {
+    echo "Nama: " . $this->nama . "<br>";
+    echo "NIM: " . $this->nim . "<br>";
+    echo "Program Studi : " . $this->prodi . "<br>";
+  }
+}
+
+// Membuat objek dari class InformasiPribadi
+$data1 = new mahasiswa("Giovanni Lucy", "121140060", "Teknik Informatika");
+$data2 = new mahasiswa("Qaisya Dwi", "121140063", "Teknik Informatika");
+
+// Memanggil metode tampilkanInfo dari objek
+$data1->showMahasiswa();
+$data2->showMahasiswa();
+?>
